da0f2fed2acfa0994f705063d7eede86d6a0d476
daonguyendev <daonguyen.dev@gmail.com>
2026-09-08 02:22:20 +0700

    Init

---
+++ category-service/src/main/java/com/session03/category_service/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.session03.category_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+@RequestMapping("/api/greeting")
+public class GreetingController {
+
+    @Value("${app.message}") // get value from config server
+    private String message;
+
+    @GetMapping
+    public String getMessage() {
+        return this.message;
+    }
+}
